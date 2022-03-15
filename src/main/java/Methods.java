16b3915105392487744c0f3c9f7c7640393ae9de
osoriojl <osoriojl@outlook.com>
2022-03-15 00:13:47 -0400

    This one is the methods section

---
+++ src/main/java/Methods.java
@@ -0,0 +1,9 @@
+public class Methods {
+    static void tryingThis() {
+        System.out.println("Wow, it actually worked!");
+    }
+
+    public static void main(String[] args) {
+        tryingThis();
+    }
+}
